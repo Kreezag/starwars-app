67383b0f3b27c5160ddb222b3169f0730feb3e24
Kuchuk Andrey <kreezag@gmail.com>
2019-09-08 16:25:43 +0300

    fix name components folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import App from './componets/App/App';
+import App from './components/App/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
