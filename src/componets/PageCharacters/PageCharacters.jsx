aa2f9593fab6e0a2bbcef4bbdf878ceb9ea66be1
Kuchuk Andrey <kreezag@gmail.com>
2019-09-08 14:55:42 +0300

    change helper functions to bottom for page-characters

--- src/componets/PageCharacters/PageCharacters.jsx
+++ src/componets/PageCharacters/PageCharacters.jsx
@@ -6,28 +6,6 @@ import './PageCharacters.less';
 
 
 
-const getId = (swapiUrlString) => {
-  const result = swapiUrlString.match('/(?:\\D*\/)(\\d*)/');
-
-  return result[1];
-};
-
-
-const createRequest = async (page) => {
-  const request = new Request(`https://swapi.co/api/people/${page ? `?&page=${JSON.stringify(page)}`: ''}`, { method: 'GET' });
-
-  return fetch(request)
-    .then((response) => response.json())
-    .then((result) => ({
-      count: result.count,
-      items: result.results
-    }))
-};
-
-
-
-
-
 
 const PageCharactersTable = ({ items = [], onClickId }) => {
   return (
@@ -52,7 +30,7 @@ const PageCharactersTable = ({ items = [], onClickId }) => {
               <td>{item.height}</td>
               <td>{item.mass}</td>
               <td>
-                <Button variant="link" onClick={onClickId(getId(item.url))} size="sm">More information</Button>
+                <Button variant="link" onClick={onClickId(getIdFromUrl(item.url))} size="sm">More information</Button>
               </td>
             </tr>
           ))}
@@ -65,7 +43,7 @@ const PageCharactersTable = ({ items = [], onClickId }) => {
 
 
 const PageCharacters = () => {
-  const [request, setRequestData] = useState({ count: 0, items: [] });
+  const [request, setRequestData] = useState({ totalCount: 0, items: [] });
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
@@ -91,16 +69,53 @@ const PageCharacters = () => {
           </div>
 
           <div className="PageCharacters__pagination">
-            <Pagination>
-              <Pagination.First/>
-              <Pagination.Item>{1}</Pagination.Item>
-              <Pagination.Last/>
-            </Pagination>
+            {hasPaginationPageCounts(request.totalCount, request.items.length)
+              ? (
+                <Pagination>
+                  <Pagination.First/>
+                  <Pagination.Item>{1}</Pagination.Item>
+                  <Pagination.Last/>
+                </Pagination>
+              ) : null}
           </div>
         </React.Fragment>
       ) : 'Loading'}
     </div>
   )
+};
+
+
+
+
+async function createRequest (page) {
+  const request = new Request(`https://swapi.co/api/people/${page ? `?&page=${JSON.stringify(page)}`: ''}`, { method: 'GET' });
+
+  return fetch(request)
+    .then((response) => response.json())
+    .then((result) => ({
+      totalCount: result.count,
+      items: result.results
+    }))
+};
+
+
+
+
+function hasPaginationPageCounts (totalCounts = 0, pageCounts = 0) {
+  if (!totalCounts || !pageCounts) {
+    return 0;
+  }
+
+  return Math.ceil(totalCounts/pageCounts);
+};
+
+
+
+
+function getIdFromUrl (swapiUrlString) {
+  const result = swapiUrlString.match('/(?:\\D*\/)(\\d*)/');
+
+  return result[1];
 };
 
 
