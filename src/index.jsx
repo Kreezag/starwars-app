834efd5ec2160a0fcec0208c5791f3e3e4c398c3
Kuchuk Andrey <kreezag@gmail.com>
2019-09-15 16:50:33 +0300

    fixed export of root component

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import Root from './components/pages/Root/Root';
+import Root from './components/pages/Root';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-
 
 render(
   <Router>
     <Root />
   </Router>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
 
 module.hot.accept();
